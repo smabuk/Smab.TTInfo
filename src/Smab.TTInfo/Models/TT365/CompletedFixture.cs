40c5337ccef79482c46e1a40b81bd94154dac12e
smabuk <2011834+smabuk@users.noreply.github.com>
2021-10-20 00:51:50 +0100

    Doubles Winners

--- src/Smab.TTInfo/Models/TT365/CompletedFixture.cs
+++ src/Smab.TTInfo/Models/TT365/CompletedFixture.cs
@@ -11,5 +11,7 @@ public record CompletedFixture : Fixture
 	public List<MatchPlayer> AwayPlayers { get; set; } = new();
 	public int Id => string.IsNullOrWhiteSpace(CardURL) ? 0 :
 		int.Parse(CardURL.Split('/').LastOrDefault() ?? "");
+	public string DoublesWinner => ForHome - HomePlayers.Sum(p => p.SetsWon) > 0 ? HomeTeam : AwayTeam;
+
 	public string Score => $"{ForHome} - {ForAway}";
 }
