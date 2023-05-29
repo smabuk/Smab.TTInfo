ff04363bca972b7bb657c33c7c94ad8605219a03
smabuk <2011834+smabuk@users.noreply.github.com>
2023-05-29 13:34:22 +0100

    GetDivisions

--- src/Smab.TTInfo/Services/TT365Reader/GetLeague.cs
+++ src/Smab.TTInfo/Services/TT365Reader/GetLeague.cs
@@ -56,54 +56,8 @@ public sealed partial class TT365Reader
 			league.CurrentSeason.Lookups = await GetLookupTables(LeagueId, league.CurrentSeason.Id);
 		}
 
-		url = $"{"https"}://www.tabletennis365.com/{LeagueId}/Tables/{league.CurrentSeason.Id}/All_Divisions";
-		doc = await LoadPage(
-		url,
-			$@"{LeagueId}_{league.CurrentSeason.Id}_Divisions_All_Divisions.html");
 
-		if (!string.IsNullOrWhiteSpace(doc.Text))
-		{
-			league.CurrentSeason.Divisions = new List<Division>();
-			foreach (HtmlNode? divTable in doc.DocumentNode.SelectNodes(@"//table"))
-			{
-				if (divTable.SelectSingleNode("caption") is null)
-				{
-					continue;
-				}
-
-				string divName = divTable.SelectSingleNode("caption")?.InnerText.Split(">").Last().Trim() ?? "";
-				Division division = new(Id: league.CurrentSeason.Lookups.DivisionLookup.Where(d => d.Name == divName).Single().Id, Name: divName);
-				league.CurrentSeason.Divisions.Add(division);
-
-				foreach (HtmlNode? teamRow in divTable.SelectNodes(@"tbody//tr"))
-				{
-					Team team = new();
-					team.Name = teamRow.ChildNodes[3].FirstChild.InnerText.Trim();
-					team.Id = league.CurrentSeason.Lookups.TeamLookup.Where(t => t.Name == team.Name).Single().Id;
-					team.ShortName = teamRow.ChildNodes[3].ChildNodes[1].InnerText.Trim().Replace("&#39;", "'").Replace("&amp;", "&");
-					team.URL = $"{"https"}://www.tabletennis365.com{teamRow.ChildNodes[3].FirstChild.FirstChild.GetAttributeValue("href", "")}";
-
-					if (int.TryParse(teamRow.SelectSingleNode(@"td[contains(@class, 'pos')]")?.InnerText ?? "0", out int leaguePosition))
-					{
-						team.LeaguePosition = leaguePosition;
-					};
-					team.Played = int.Parse(teamRow.SelectSingleNode(@"td[contains(@class, 'played')]")?.InnerText ?? "0");
-					team.Won = int.Parse(teamRow.SelectSingleNode(@"td[contains(@class, 'won')]")?.InnerText ?? "0");
-					team.Drawn = int.Parse(teamRow.SelectSingleNode(@"td[contains(@class, 'drawn')]")?.InnerText ?? "0");
-					team.Lost = int.Parse(teamRow.SelectSingleNode(@"td[contains(@class, 'lost')]")?.InnerText ?? "0");
-					team.SetsFor = int.Parse(teamRow.SelectSingleNode(@"td[contains(@class, 'setsFor')]")?.InnerText ?? "0");
-					team.SetsAgainst = int.Parse(teamRow.SelectSingleNode(@"td[contains(@class, 'setsAgainst')]")?.InnerText ?? "0");
-					team.PointsAdjustment = int.Parse(teamRow.SelectSingleNode(@"td[contains(@class, 'pointAdj')]")?.InnerText ?? "0");
-					team.Points = int.Parse(teamRow.SelectSingleNode(@"td[contains(@class, 'points')]")?.InnerText ?? "0");
-
-					team.DivisionName = divName;
-
-					division.Teams.Add(team);
-				}
-
-			}
-
-		}
+		league.CurrentSeason.Divisions = await GetDivisions(LeagueId, league.CurrentSeason.Id);
 
 		jsonString = JsonSerializer.Serialize(league);
 		_ = SaveFile(jsonString, $"league_{LeagueId}.json");
