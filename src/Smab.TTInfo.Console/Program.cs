201c4af1e038869591b7293d7a5ce556dd7a5dce
smabuk <2011834+smabuk@users.noreply.github.com>
2023-06-03 17:57:14 +0100

    May be too much info for 1 player line

--- src/Smab.TTInfo.Console/Program.cs
+++ src/Smab.TTInfo.Console/Program.cs
@@ -1,12 +1,10 @@
-﻿using System.Globalization;
-
-int year = DateTime.Today.AddMonths(-8).Year;
+﻿int year = DateTime.Today.AddMonths(-8).Year;
 string leagueId = "Reading";
 string seasonId = $"Senior_{year}-{year + 1 - 2000}";
 string? searchName = null; 
 
 List<LookupTables> allLookupTables = new();
-List<Division> allDivisions = new();
+List<Division>     allDivisions    = new();
 
 Console.WriteLine("Table Tennis 365 reader");
 
@@ -24,7 +22,7 @@ if (args.Length == 1) {
 TT365Reader tt365 = new()
 {
 	CacheFolder = """c:\temp\tt365\cache""",
-	CacheHours = 100000,
+	CacheHours = 10_000_000,
 };
 
 League? league = await tt365.GetLeague(leagueId);
@@ -44,6 +42,7 @@ LookupTables lookupTables = await tt365.GetLookupTables(leagueId, seasonId);
 allLookupTables.Add(lookupTables);
 List<Division> divisions = await tt365.GetDivisions(leagueId, seasonId);
 allDivisions.AddRange(divisions);
+
 foreach (Division division in divisions) {
 	Console.WriteLine($"  {division.Name}");
 	foreach (Team team in division.Teams) {
@@ -56,33 +55,31 @@ foreach (Division division in divisions) {
 			Console.Write($" *** {ex.Message}");
 		}
 		finally {
-			Console.WriteLine($"  {newTeam.Captain,-20}");
+			Console.WriteLine($"  {TT365Reader.FixPlayerName(newTeam.Captain),-20}");
 		}
-		//if (newTeam.Name.ToLowerInvariant().Contains("peace") || newTeam.Name.ToLowerInvariant().Contains("olop")) {
-			foreach (Player player in newTeam.Players?.OrderByDescending(p => p.WinPercentage).ToList() ?? new List<Player>()) {
-				Console.WriteLine($"         {player.Name,-25} {player.Played,6} {(int)player.WinPercentage,3}%");
-				if (searchName is not null && player.Name.ToLowerInvariant().Contains(searchName)) {
-					Player p2 = await tt365.GetPlayerStats(leagueId, player, seasonId) ?? new();
-					ConsoleColor color = Console.ForegroundColor;
-					foreach (PlayerResult playerResult in p2.PlayerResults.Where(pr => pr.PlayerTeamName == team.Name).OrderBy(pr => pr.Date)) {
-						string dateString = playerResult.Date.ToString("dd MMM");
-						dateString = dateString.Length <= 6 ? dateString : dateString[..6];
-						Console.ForegroundColor = playerResult.Result.ToLowerInvariant() switch
-						{
-							"win"  => ConsoleColor.Green,
-							"loss" => ConsoleColor.Red,
-							_      => Console.ForegroundColor,
-						};
-						Console.WriteLine($"            {dateString, -6}  {(playerResult.ResultReason.Any() ? "*" : ""),1}{playerResult.Result,-4}  {playerResult.RankingDiffString,3}  {playerResult.Opponent.Name,-24}   {playerResult.OpponentTeam,-30}  {playerResult.GameScore,3}  {playerResult.Scores}");
-						if (playerResult.ResultReason.Any() )
-						{
-							Console.WriteLine($"                     {playerResult.ResultReason}");
-						}
+		foreach (Player player in newTeam.Players?.OrderByDescending(p => p.WinPercentage).ToList() ?? new List<Player>()) {
+			Console.WriteLine($"         {TT365Reader.FixPlayerName(player.Name),-25} {player.Played,6} {(int)player.WinPercentage,3}%");
+			if (searchName is not null && player.Name.ToLowerInvariant().Contains(searchName)) {
+				Player p2 = await tt365.GetPlayerStats(leagueId, player, seasonId) ?? new();
+				ConsoleColor color = Console.ForegroundColor;
+				foreach (PlayerResult playerResult in p2.PlayerResults.Where(pr => pr.PlayerTeamName == team.Name).OrderBy(pr => pr.Date)) {
+					string dateString = playerResult.Date.ToString("dd MMM");
+					dateString = dateString.Length <= 6 ? dateString : dateString[..6];
+					Console.ForegroundColor = playerResult.Result.ToLowerInvariant() switch
+					{
+						"win"  => ConsoleColor.Green,
+						"loss" => ConsoleColor.Red,
+						_      => Console.ForegroundColor,
+					};
+					Console.WriteLine($"          {dateString, -6} {(playerResult.ResultReason.Any() ? "*" : ""),1}{playerResult.Result.FirstOrDefault(),1}  {playerResult.RankingDiffString,3}  {TT365Reader.FixPlayerName(playerResult.Opponent.Name),-24}   {playerResult.OpponentTeam,-30}  {playerResult.GameScore,3}  {playerResult.Scores}");
+					if (playerResult.ResultReason.Any() )
+					{
+						Console.WriteLine($"                  {playerResult.ResultReason}");
 					}
-					Console.ForegroundColor = color;
 				}
+				Console.ForegroundColor = color;
 			}
-		//}
+		}
 	}
 }
 
