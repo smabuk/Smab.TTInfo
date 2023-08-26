d1a3bbe7581f99511db83cf04cb841dbf505ccb5
smabuk <2011834+smabuk@users.noreply.github.com>
2023-08-26 13:09:57 +0100

    Blazor 8 template and appearance

--- src/Smab.TTInfo.Server/Program.cs
+++ src/Smab.TTInfo.Server/Program.cs
@@ -1,15 +1,17 @@
-﻿using System.Reflection;
+﻿using System.Globalization;
+using System.Reflection;
 
 using Smab.Shared.Helpers;
+using Smab.TTInfo.Server;
 using Smab.TTInfo.Server.EndPoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages()
-	.AddJsonOptions(options => options.JsonSerializerOptions.AddDateOnlyAndTimeOnlyConverters());
+builder.Services.AddRazorComponents();
+/*	.AddJsonOptions(options => options.JsonSerializerOptions.AddDateOnlyAndTimeOnlyConverters())*/;
 
-builder.Services.AddServerSideBlazor();
+builder.Services.AddLocalization();
 
 builder.Services.AddScoped<ITT365Reader, TT365Reader>(
 	tt => new()
@@ -24,7 +26,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-	app.UseExceptionHandler("/Error");
+	_ = app.UseExceptionHandler("/Error");
 	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 	app.UseHsts();
 }
@@ -33,13 +35,14 @@ app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 
-app.UseRouting();
+app.MapRazorComponents<App>();
 
-
-app.MapControllers();
-
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
+app.UseRequestLocalization(
+	new RequestLocalizationOptions()
+		.SetDefaultCulture("en-GB")
+		.AddSupportedCultures("en-GB")
+		.AddSupportedUICultures(cultures)
+	);
 
 app.MapCalendarEndPoints();
 app.MapGroup("/tt")
@@ -55,13 +58,29 @@ static partial class Program
 							.GetName().Name ?? "No assembly name";
 	public static Version Version { get; set; } = typeof(Program).Assembly
 							.GetName().Version ?? new();
-	public static string ProductVersion { get; set; } = typeof(Program).Assembly
+	public static string ProductVersion { get; set; } =
+		VersionWithoutGuid(typeof(Program).Assembly
 							.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
-							.InformationalVersion ?? "";
+							.InformationalVersion ?? ""
+		);
 
 	public static string SmabTTInfoName { get; set; } = typeof(ITT365Reader).Assembly
 							.GetName().Name ?? "No assembly name";
-	public static string SmabTTInfoVersion { get; set; } = typeof(ITT365Reader).Assembly
+	public static string SmabTTInfoVersion { get; set; } =
+		VersionWithoutGuid(typeof(ITT365Reader).Assembly
 							.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
-							.InformationalVersion ?? "";
+							.InformationalVersion ?? ""
+		);
+	public static string FrameworkVersion { get; } = System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription;
+
+	private static readonly string[] cultures = CultureInfo
+		.GetCultures(CultureTypes.AllCultures)
+		.Select(c => c.Name)
+		.ToArray();
+
+	private static string VersionWithoutGuid(string version)
+	{
+		int indexOfPlus = version.IndexOf('+');
+		return (indexOfPlus > 0) ? version[..indexOfPlus] : version;
+	}
 }
