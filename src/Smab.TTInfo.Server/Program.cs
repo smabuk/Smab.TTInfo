316ea2c41c48ae03cdd4d8e98c66434a27694f20
smabuk <2011834+smabuk@users.noreply.github.com>
2023-09-15 18:38:16 +0100

    Narrow sidebar width to 200px

--- src/Smab.TTInfo.Server/Program.cs
+++ src/Smab.TTInfo.Server/Program.cs
@@ -54,7 +54,7 @@ app.Run();
 
 static partial class Program
 {
-	public static string SiteName { get; set; } = "Table Tennis Info";
+	public static string SiteName { get; set; } = "🏓 Info";
 	public static string Name { get; set; } = typeof(Program).Assembly
 							.GetName().Name ?? "No assembly name";
 	public static Version Version { get; set; } = typeof(Program).Assembly
