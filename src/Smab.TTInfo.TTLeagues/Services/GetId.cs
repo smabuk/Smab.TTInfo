6140e63c2c651613a15f434da0cda0c3c7337c1f
smabuk <2011834+smabuk@users.noreply.github.com>
2023-10-02 19:03:51 +0100

    TTLeagues with multiple competitions ongoing

--- src/Smab.TTInfo.TTLeagues/Services/GetId.cs
+++ src/Smab.TTInfo.TTLeagues/Services/GetId.cs
@@ -4,9 +4,7 @@ public sealed partial class TTLeaguesReader
 {
 	internal async Task<int?> GetId(string name, LookupType type, string leagueId, int competitionId)
 	{
-		string fileName = $"{leagueId}_{competitionId}_lookup_tables.json";
-
-		List<LookupValue>? lookup = await GetLookupTables(leagueId, competitionId);
+		List<LookupValue>? lookup = await GetLookupTables(leagueId);
 
 		return lookup
 			?.FirstOrDefault(l => l.Type == type && l.Name.Equals(name, StringComparison.InvariantCultureIgnoreCase))
--- src/Smab.TTInfo.TTLeagues/Services/GetLookupTables.cs
+++ src/Smab.TTInfo.TTLeagues/Services/GetLookupTables.cs
@@ -1,34 +1,34 @@
-﻿namespace Smab.TTInfo.TTLeagues.Services;
+﻿using Smab.TTInfo.TTLeagues.Models.TTLeagues;
+
+namespace Smab.TTInfo.TTLeagues.Services;
 
 public sealed partial class TTLeaguesReader
 {
-	internal async Task<List<LookupValue>> GetLookupTables(string leagueId, int competitionId)
+	internal async Task<List<LookupValue>> GetLookupTables(string leagueId)
 	{
-		string fileName = $"{leagueId}_{competitionId}_lookup_tables.json";
 		string fileNameWithoutCompetitionId = $"{leagueId}_lookup_tables.json";
 
 		List<LookupValue>? lookup = await LoadJsonAsync<List<LookupValue>>(
 			leagueId,
 			null,
-			fileName);
+			fileNameWithoutCompetitionId);
 
 		if (lookup is null) {
 			lookup = [];
 			League? league = await GetLeague(leagueId);
 			foreach (Competition competition in league?.CurrentCompetitions ?? []) {
 				lookup.Add(new(LookupType.Competition, competition.Id, competition.Name));
-			}
-			List<Division> divisions = await GetDivisions(leagueId, competitionId);
-			foreach (Division division in divisions) {
-				lookup.Add(new(LookupType.Division ,division.Id, division.Name));
-				List<TeamStanding>? teamStandings = await GetDivisionStandings(division.Id, leagueId);
-				foreach (TeamStanding team in teamStandings ?? []) {
-					lookup.Add(new(LookupType.Team, team.TeamId, team.Name));
+				List<Division> divisions = await GetDivisions(leagueId, competition.Id);
+				foreach (Division division in divisions) {
+					lookup.Add(new(LookupType.Division, division.Id, division.Name));
+					List<TeamStanding>? teamStandings = await GetDivisionStandings(division.Id, leagueId);
+					foreach (TeamStanding team in teamStandings ?? []) {
+						lookup.Add(new(LookupType.Team, team.TeamId, team.Name));
+					}
 				}
+				_ = SaveFile(JsonSerializer.Serialize(lookup), $"{leagueId}_{competition.Id}_lookup_tables.json");
 			}
-			string json = JsonSerializer.Serialize(lookup);
-			_ = SaveFile(json, fileName);
-			_ = SaveFile(json, fileNameWithoutCompetitionId);
+			_ = SaveFile(JsonSerializer.Serialize(lookup), fileNameWithoutCompetitionId);
 		}
 
 		return lookup;
