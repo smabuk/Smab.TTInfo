61729a389e4304755d47c79b9095e18cf44d89b1
smabuk <2011834+smabuk@users.noreply.github.com>
2026-08-14 19:57:11 +0100

    v1.3.32 Improve weeksPlayed calculation; remove unused using
    GetSummaryData.cs now dynamically determines valid weeks by checking for non-zero data across weeks 1-6, and calculates weeksPlayed per player based on actual data presence, increasing accuracy. Also removed an unused System.Text using directive from OlopPremierSummary.razor.

--- src/Smab.TTInfo.Olop/Helpers/GetSummaryData.cs
+++ src/Smab.TTInfo.Olop/Helpers/GetSummaryData.cs
@@ -16,14 +16,39 @@ public static partial class ExcelPackageHelpers
 			const int colPercentage = 3;
 			const int colPoints = 4;
 
+			const int WEEKS_START = 6;
+
+			ExcelWorksheet summarySheet = package.Workbook.Worksheets[SUMMARY_SHEET_NAME];
 			ExcelTable summaryTable = package.Workbook.Worksheets[SUMMARY_SHEET_NAME].Tables[SUMMARY_TABLE_NAME];
+
+			List<int> validWeeks = [];
+			for (int weekNo = 1; weekNo <= 6; weekNo++) {
+				bool foundNonZero = false;
+				for (int i = 0; i < summaryTable.DataRows.Count(); i++) {
+					int rowIdx = i + 4; // Data rows start at row
+					string ptsString = summarySheet.Cells[rowIdx, WEEKS_START + weekNo - 1].Text;
+					if (string.IsNullOrWhiteSpace(ptsString)) {
+						break;
+					}
+
+					_ = double.TryParse(ptsString, out double weekPoints);
+					if (weekPoints != 0.0) {
+						foundNonZero = true;
+						break;
+					}
+				}
+
+				if (foundNonZero is true) {
+					validWeeks.Add(weekNo);
+				}
+			}
 
 			foreach (ExcelTableRow dataRow in summaryTable.DataRows) {
 				ExcelRangeBase[] values = [.. dataRow.RowRange];
 				string name = values[colName].Text;
 				if (!string.IsNullOrWhiteSpace(name)) {
 					int rank = Convert.ToInt32(values[colRank].Value);
-					int weeksPlayed = Convert.ToInt32(values[colWeeksPlayed].Value);
+					int weeksPlayed = validWeeks.Select(weekNo => string.IsNullOrWhiteSpace(values[WEEKS_START + weekNo - 2].Text) ? 0 : 1).Sum();
 					double percentage = Convert.ToDouble(values[colPercentage].Value) * 100;
 					double points = Convert.ToDouble(values[colPoints].Value);
 					yield return new SummaryTableItem(rank, new NamedPlayer(name), weeksPlayed, percentage, points);
