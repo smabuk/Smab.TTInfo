4deff271f896d97cb03b503b41ce24ecd0632cf8
smabuk <2011834+smabuk@users.noreply.github.com>
2026-09-12 11:37:27 +0100

    v1.4.30 Add season-based league summaries and UI improvements (no extra caching yet)
    Added support for displaying league summaries by season via an optional SeasonId route parameter. Introduced a loading state for data fetching. Refactored to fetch divisions based on the selected season, using the API for non-current seasons. Updated the UI to show league and season info, replaced the season dropdown with navigation links, and improved CSS for headings and links. Updated code-behind to handle SeasonId, fetch divisions, and manage loading state.

--- src/Smab.TTInfo.TT365/Pages/LeagueSummary.razor.cs
+++ src/Smab.TTInfo.TT365/Pages/LeagueSummary.razor.cs
@@ -1,24 +1,36 @@
 ﻿namespace Smab.TTInfo.TT365.Pages;
 public partial class LeagueSummary
 {
-	[EditorRequired]
-	[Parameter]
-	public string LeagueId { get; set; } = "";
+	[EditorRequired] [Parameter] public string LeagueId { get; set; } = "";
+	[EditorRequired] [Parameter] public string? SeasonId { get; set; } = null;
 
 	private string LeagueName { get; set; } = "";
 	private League? League { get; set; }
 
-	private string currentSeason = "";
-	private List<string> seasons = [];
+	// private string CurrentSeasonName = "";
+	private TT365LeagueId leagueId;
+	private List<Division> divisions = [];
+	private bool isLoading = false;
 
 	protected override async Task OnParametersSetAsync()
 	{
+		isLoading = true;
+
 		if (LeagueId is not null) {
+			leagueId = (TT365LeagueId)LeagueId;
 			LeagueName = LeagueId;
 			League = await _tt365.GetLeague((TT365LeagueId)LeagueId);
-			LeagueName = League?.Name ?? "";
-			currentSeason = League?.CurrentSeason.Name ?? "";
-			seasons = League?.Seasons.Select(s => s.Name).Append(currentSeason).OrderByDescending(s => s).ToList() ?? [];
+			if (League is null) { return; }
+
+			LeagueName = League.Name;
+			SeasonId = string.IsNullOrWhiteSpace(SeasonId) ? League.CurrentSeason.Name ?? "" : SeasonId;
+			if (SeasonId != League.CurrentSeason.Name) {
+				divisions = [.. await _tt365.GetDivisions((TT365LeagueId)LeagueId, (TT365SeasonId)SeasonId)];
+			} else {
+				divisions = [.. League.CurrentSeason.Divisions];
+			}
 		}
+
+		isLoading = false;
 	}
 }
